6be682eb1a5ca933136fcbfe090146bdc83d0a4b
Russel Arthur <concrain@concrain.lan>
2018-05-13 14:17:53 -0700

    global variables

--- install-openshift.sh
+++ install-openshift.sh
@@ -2,27 +2,34 @@
 
 ## see: https://youtu.be/aqXSbDZggK4
 
-export DOMAIN=${DOMAIN:="$(curl ipinfo.io/ip).nip.io"}
-export USERNAME=${USERNAME:="$(whoami)"}
-export PASSWORD=${PASSWORD:=password}
-export VERSION=${VERSION:="v3.9.0"}
+# set exports
+export DOMAIN="concrain.io"
+export USERNAME="zeus"
+export PASSWORD="F\$0ciety"
+export DISK="/dev/sda"
+export VERSION="v3.9.0"
+v=$VERSION
+export SUBVERSION=${v%.*}
+export RELEASEVERSION=${v:1:3}
+export DOCKERVERSION="1.13.1"
 
-
-export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
+export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/concrain/installcentos/master"}
 
 export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
 export API_PORT=${API_PORT:="8443"}
 
 echo "******"
-echo "* Your domain is $DOMAIN "
-echo "* Your IP is $IP "
-echo "* Your username is $USERNAME "
-echo "* Your password is $PASSWORD "
+echo "* Your domain is: $DOMAIN "
+echo "* Your IP is: $IP "
+echo "* Your username is: $USERNAME "
+echo "* Your password is: $PASSWORD "
 echo "* OpenShift version: $VERSION "
+echo "* OpenShift sub-version: $SUBVERSION"
+echo "* Docker version: $DOCKERVERSION"
 echo "******"
 
 # install the following base packages
-yum install -y  wget git zile nano net-tools docker-1.13.1\
+yum install -y  wget git zile nano net-tools docker-$DOCKERVERSION\
 				bind-utils iptables-services \
 				bridge-utils bash-completion \
 				kexec-tools sos psacct openssl-devel \
@@ -36,7 +43,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 # Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
 sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 
-systemctl | grep "NetworkManager.*running" 
+systemctl | grep "NetworkManager.*running"
 if [ $? -eq 1 ]; then
 	systemctl start NetworkManager
 	systemctl enable NetworkManager
@@ -47,15 +54,15 @@ yum -y --enablerepo=epel install ansible pyOpenSSL
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
 
-cd openshift-ansible && git fetch && git checkout release-3.9 && cd ..
+cd openshift-ansible && git fetch && git checkout release-$RELEASEVERSION && cd ..
 
 cat <<EOD > /etc/hosts
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN}  
+${IP}		$(hostname) console console.${DOMAIN}
 EOD
 
-if [ -z $DISK ]; then 
+if [ -z $DISK ]; then
 	echo "Not setting the Docker storage."
 else
 	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
